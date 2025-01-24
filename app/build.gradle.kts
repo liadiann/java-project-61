874b893484eab48d2f2c8b75c6c97fc0ce5032a6
Julia Kon <kon1976y@gmail.com>
2025-01-24 18:48:42 +0300

    update build.gradle.kts(add application)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    application
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
@@ -14,6 +15,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 tasks.test {
