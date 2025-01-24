941343130ca3e3dc4c9bb32964c30189243074e8
Julia Kon <kon1976y@gmail.com>
2025-01-24 19:55:28 +0300

    add Cli.java and update App.java

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+class Cli {
+    public static void greeting() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = sc.next();
+        System.out.println("Hello, " + name + "!");
+        sc.close();
+    }
+}
