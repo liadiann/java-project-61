eef2b8e519b3a8560787f6a3f61165d040ddf82f
Julia Kon <kon1976y@gmail.com>
2025-01-25 19:47:26 +0300

    add game "even"

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,24 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit\nYour choice: ");
+        String choice = sc.next();
+        switch (choice) {
+            case "1":
+                Cli.greet();
+                break;
+            case "2":
+                Even.play();
+                break;
+            case "0":
+                break;
+            default:
+                System.out.println("Incorrect input");
+        }
+        sc.close();
     }
 }
