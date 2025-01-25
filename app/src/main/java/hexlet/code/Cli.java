32febf814d9ddc22aa50d52c37ea9a9aabec9cec
Julia Kon <kon1976y@gmail.com>
2025-01-25 19:46:19 +0300

    update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 class Cli {
-    public static void greeting() {
+    public static String greet() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String name = sc.next();
         System.out.println("Hello, " + name + "!");
-        sc.close();
+        return name;
     }
 }
